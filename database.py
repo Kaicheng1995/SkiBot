2b46dfd6076fa2e4c9df641348956e0460c0843d
KAI <ares.jia@hotmail.com>
2021-12-06 23:12:49 -0800

    Weather Uploaded Complete!

--- database.py
+++ database.py
@@ -1,6 +1,14 @@
 from resort import Resort
 import requests
 import json
+from datetime import datetime, date, timedelta
+
+
+'''
+***************************
+    1/ GET WEATHER DATA
+***************************
+'''
 
 APP_ID = "39eba088"
 APP_KEY = "fe507959f9cfd3f54af726fc36e63d9a"
@@ -27,28 +35,58 @@ Raw_Data = []
 for resort in Resorts:
     Raw_Data.append(request(resort.resort_id, APP_ID, APP_KEY).json())
 
+# get current date and incoming 7 days' dates
+today = date.today()
+Seven_Days = []
+for i in range(1, 8):
+    next_day = today + timedelta(days=i)
+    Seven_Days.append(next_day.strftime("%d/%m/%Y"))
+print(Seven_Days)
 
-#
+
+# populate the core weather data into all resort objects
+def set_weather():
+    for d in range(len(Seven_Days)):
+        for r in range(len(Resorts)):
+            set_weather_helper(Seven_Days[d], Raw_Data[r], Resorts[r], d)
 
 
+'''
+@forecast_date: the date of which weather to be forecasted
+@raw_data: the full weather data without processed
+@resort: the particular resort which weather to be forecasted
+@kth_day: the Kth day need to be forecasted from now
+'''
 
 
-
-# take a raw data formatted in dict and return the core data
-# update the "weather" template in real time
-def set_weather(raw_data, resort):
+# abstract real-time core weather data and update in objects
+def set_weather_helper(forecast_date, raw_data, each_resort, kth_day):
     for day in raw_data["forecast"]:
-        if "07/12/2021" in day.values():
+        if forecast_date in day.values():
+            # choose date between 10am - 12am
             if "10:00" in day.values() \
-                    or "11:00" in day.values()\
+                    or "11:00" in day.values() \
                     or "12:00" in day.values():
-                for key in resort.weather:
-                    if key in day:
-                        resort.weather[key] = day[key]
-                    else:
-                        resort.weather[key] = day["base"][key]
+                # update core weather info on Kth day
+                each_resort.weather[kth_day].set_weather(
+                    day["date"],
+                    day["base"]["wx_desc"],
+                    day["snow_in"],
+                    day["rain_in"],
+                    day["vis_mi"],
+                    day["slp_in"],
+                    day["base"]["temp_f"],
+                    day["base"]["windspd_mph"]
+                )
 
 
 # populate the core weather data into resort object
-for i in range(5):
-    set_weather(Raw_Data[i], Resorts[i])
+set_weather()
+
+
+
+'''
+***************************
+    2/ GET  DATA
+***************************
+'''
--- weather.py
+++ weather.py
@@ -1,5 +1,6 @@
 class Weather:
 
+    # initialize an empty object
     def __init__(self):
         self.date = ""  # Date
         self.wx_desc = ""  # Weather Description
@@ -9,3 +10,33 @@ class Weather:
         self.slp_in = 0.0  # Humidity Level (%)
         self.temp_f = 0.0  # Temperature (F)
         self.windspd_mph = 0.0  # Wind Speed (mph)
+
+    # set value to weather object
+    def set_weather(self,
+                    date,
+                    wx_desc,
+                    snow_in,
+                    rain_in,
+                    vis_mi,
+                    slp_in,
+                    temp_f,
+                    windspd_mph):
+        self.date = date
+        self.wx_desc = wx_desc
+        self.snow_in = snow_in
+        self.rain_in = rain_in
+        self.vis_mi = vis_mi
+        self.slp_in = slp_in
+        self.temp_f = temp_f
+        self.windspd_mph = windspd_mph
+
+    def __str__(self):
+        out = "Date: " + str(self.date) + "\n" + \
+              "Weather: " + str(self.wx_desc) + "\n" + \
+              "Snowfall (inch): " + str(self.snow_in) + "\n" + \
+              "Rainfall (inch): " + str(self.rain_in) + "\n" + \
+              "Visibility (inch): " + str(self.vis_mi) + "\n" + \
+              "Humidity Level (miles): " + str(self.slp_in) + "\n" + \
+              "Temperature (F): " + str(self.temp_f) + "\n" + \
+              "Wind Speed: (mhp): " + str(self.windspd_mph)
+        return out
